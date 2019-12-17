d012a49ac80830ecf23112a3ba3a6324728b9fe3
daibingh <daibingh@gmail.com>
2019-12-17 23:00:37 +0800

    sogou options

--- demo.py
+++ demo.py
@@ -60,7 +60,7 @@ def sogou_score(browser, opts):
     text = browser.find_element_by_class_name('results').text
     return dict(zip(opts, map(lambda t: text.count(t), opts)))
 
-@run_time
+
 def main():
     uid = uuid.uuid4().hex
     ret = screencap()
@@ -74,33 +74,41 @@ def main():
         print(">>>> 搜索的关键词是：{}".format(search_text))
         log.info("{}: {}".format(uid, search_text))
 
-        elem = browser.find_element_by_id("kw")
+        if F.use_sogou:
         browser_2.get('https://www.sogou.com/')
         elem_2 = browser_2.find_element_by_id("query")
-        elem.clear()
         elem_2.clear()
-        elem.send_keys(search_text)
         elem_2.send_keys(search_text)
-        elem.send_keys(Keys.RETURN)
         elem_2.send_keys(Keys.RETURN)
-        time.sleep(1)
+
+        elem = browser.find_element_by_id("kw")
+        elem.clear()
+        elem.send_keys(search_text)
+        elem.send_keys(Keys.RETURN)
+
+        time.sleep(F.wait_time)
+
+        if F.use_sogou:
+            opt_score_2 = sogou_score(browser_2, search_text.split('?')[-1].strip().split(' '))
+            sogou_ans, sogou_ans_bak = max(opt_score_2, key=opt_score.get), min(opt_score_2, key=opt_score.get)
         opt_score = baidu_score(browser, search_text.split('?')[-1].strip().split(' '))
-        opt_score_2 = sogou_score(browser_2, search_text.split('?')[-1].strip().split(' '))
+        baidu_ans, baidu_ans_bak = max(opt_score, key=opt_score.get), min(opt_score, key=opt_score.get)
 
         print('>>>> 百度打分: {}'.format(opt_score))
-        print('>>>> 搜狗打分: {}'.format(opt_score_2))
-        print('\n')
-        print(">>>> 百度推荐答案: {}, 备选(否定题目): {}".format(max(opt_score, key=opt_score.get), min(opt_score, key=opt_score.get)))
-        print(">>>> 搜狗推荐答案: {}, 备选(否定题目): {}".format(max(opt_score_2, key=opt_score_2.get), min(opt_score_2, key=opt_score_2.get)))
-        print('------------------------------------------------------')
         log.info("{}: 百度打分 {}".format(uid, opt_score))
-        log.info("{}: 搜狗打分 {}".format(uid, opt_score_2))
+        if F.use_sogou:
+            print('>>>> 搜狗打分: {}'.format(opt_score_2))
+            log.info("{}: 搜狗打分 {}".format(uid, opt_score_2))
+        print('\n')
+        print(">>>> 百度推荐答案: {}, 备选(否定题目): {}".format(baidu_ans, baidu_ans_bak))
+        if F.use_sogou:
+            print(">>>> 搜狗推荐答案: {}, 备选(否定题目): {}".format(sogou_ans, sogou_ans_bak))
+        print('------------------------------------------------------')
 
         if F.use_wx:
-            msg = "百度推荐答案: {}, 备选(否定题目): {}\n搜狗推荐答案: {}, 备选(否定题目): {}".format(
-                    max(opt_score, key=opt_score.get), min(opt_score, key=opt_score.get),
-                    max(opt_score_2, key=opt_score_2.get), min(opt_score_2, key=opt_score_2.get)
-                    )
+            msg = "百度推荐答案: {}, 备选(否定题目): {}".format(baidu_ans, baidu_ans_bak)
+            if F.use_sogou:
+                msg += "\n搜狗推荐答案: {}, 备选(否定题目): {}".format(sogou_ans, sogou_ans_bak)
             group.send(msg)
 
     except Exception as e:
@@ -122,7 +130,7 @@ def test():
     elem.send_keys(res)
     elem.send_keys(Keys.RETURN)
 
-
+@run_time
 def test2():
     search_text_list = ["2019年8月发行的新版第五套人民币没有以下 哪种面额的纸币? 100元 50元 1元",
                     "进博会吉祥物叫什么名字? 招财 进宝 来福",
@@ -135,31 +143,39 @@ def test2():
     search_text = np.random.choice(search_text_list)
     print(">>>> 搜索的关键词是：{}".format(search_text))
 
+    if F.use_sogou:
+        browser_2.get('https://www.sogou.com/')
+        elem_2 = browser_2.find_element_by_id("query")
+        elem_2.clear()
+        elem_2.send_keys(search_text)
+        elem_2.send_keys(Keys.RETURN)
+
     elem = browser.find_element_by_id("kw")
-    browser_2.get('https://www.sogou.com/')
-    elem_2 = browser_2.find_element_by_id("query")
     elem.clear()
-    elem_2.clear()
     elem.send_keys(search_text)
-    elem_2.send_keys(search_text)
     elem.send_keys(Keys.RETURN)
-    elem_2.send_keys(Keys.RETURN)
-    time.sleep(1)
+
+    time.sleep(F.wait_time)
+
+    if F.use_sogou:
+        opt_score_2 = sogou_score(browser_2, search_text.split('?')[-1].strip().split(' '))
+        sogou_ans, sogou_ans_bak = max(opt_score_2, key=opt_score.get), min(opt_score_2, key=opt_score.get)
     opt_score = baidu_score(browser, search_text.split('?')[-1].strip().split(' '))
-    opt_score_2 = sogou_score(browser_2, search_text.split('?')[-1].strip().split(' '))
+    baidu_ans, baidu_ans_bak = max(opt_score, key=opt_score.get), min(opt_score, key=opt_score.get)
 
     print('>>>> 百度打分: {}'.format(opt_score))
-    print('>>>> 搜狗打分: {}'.format(opt_score_2))
+    if F.use_sogou:
+        print('>>>> 搜狗打分: {}'.format(opt_score_2))
     print('\n')
-    print(">>>> 百度推荐答案: {}, 备选(否定题目): {}".format(max(opt_score, key=opt_score.get), min(opt_score, key=opt_score.get)))
-    print(">>>> 搜狗推荐答案: {}, 备选(否定题目): {}".format(max(opt_score_2, key=opt_score_2.get), min(opt_score_2, key=opt_score_2.get)))
+    print(">>>> 百度推荐答案: {}, 备选(否定题目): {}".format(baidu_ans, baidu_ans_bak))
+    if F.use_sogou:
+        print(">>>> 搜狗推荐答案: {}, 备选(否定题目): {}".format(sogou_ans, sogou_ans_bak))
     print('------------------------------------------------------')
 
     if F.use_wx:
-        msg = "百度推荐答案: {}, 备选(否定题目): {}\n搜狗推荐答案: {}, 备选(否定题目): {}".format(
-                max(opt_score, key=opt_score.get), min(opt_score, key=opt_score.get),
-                max(opt_score_2, key=opt_score_2.get), min(opt_score_2, key=opt_score_2.get)
-                )
+        msg = "百度推荐答案: {}, 备选(否定题目): {}".format(baidu_ans, baidu_ans_bak)
+        if F.use_sogou:
+            msg += "\n搜狗推荐答案: {}, 备选(否定题目): {}".format(sogou_ans, sogou_ans_bak)
         group.send(msg)
 
 
@@ -171,7 +187,8 @@ def onKeyboardEvent(event):
             main()
     elif event.Key == 'Q':
         browser.quit()
-        browser_2.quit()
+        if F.use_sogou:
+            browser_2.quit()
         sys.exit()
     return True
 
@@ -182,6 +199,8 @@ if __name__ == '__main__':
     args.add_argument('--use_phone', action='store_true')
     args.add_argument('--debug', action='store_true')
     args.add_argument('--use_wx', action='store_true')
+    args.add_argument('--use_sogou', action='store_true')
+    args.add_argument('--wait_time', type=float, default=.25)
 
     F = args.parse_args()
 
@@ -196,8 +215,9 @@ if __name__ == '__main__':
     browser = webdriver.Chrome(chromedriver_path)
     browser.get(search_engine)
 
-    browser_2 = webdriver.Chrome(chromedriver_path)
-    browser_2.get('https://www.sogou.com/')
+    if F.use_sogou:
+        browser_2 = webdriver.Chrome(chromedriver_path)
+        browser_2.get('https://www.sogou.com/')
 
     if F.use_wx:
         # 初始化机器人，扫码登陆
