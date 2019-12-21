8f5747955309c80c94a1e78941869b3f9705f80c
daibingh <daibingh@gmail.com>
2019-12-21 11:27:15 +0800

    xxx

--- demo_baidu.py
+++ demo_baidu.py
@@ -4,7 +4,7 @@ import subprocess
 import time
 import cv2
 import numpy as np
-from utils import run_time, ocr, ocr2, Logger, crop_ocr, toutiao_score
+from utils import run_time, ocr, ocr2, Logger, toutiao_score
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
